d7d2988b6f9e233e4eb3f7f62fc96ad84056f983
Rohtash Singh Lakra <rohtash.singh@gmail.com>
2018-02-16 08:14:27 -0800

    Updated Copyright Information.

--- src/com/rslakra/mock/logger/MockLogManager.java
+++ src/com/rslakra/mock/logger/MockLogManager.java
@@ -1,6 +1,25 @@
-/***************************************************************************
- * Copyright (C) RSLakra 2009
- **************************************************************************/
+/******************************************************************************
+ * Copyright (C) Devamatre Inc. 2009-2018.
+ * 
+ * This code is licensed to Devamatre under one or more contributor license 
+ * agreements. The reproduction, transmission or use of this code or the 
+ * snippet is not permitted without prior express written consent of Devamatre. 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the license is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied and the 
+ * offenders will be liable for any damages. All rights, including  but not
+ * limited to rights created by patent grant or registration of a utility model 
+ * or design, are reserved. Technical specifications and features are binding 
+ * only insofar as they are specifically and expressly agreed upon in a written 
+ * contract.
+ * 
+ * You may obtain a copy of the License for more details at:
+ *      http://www.devamatre.com/licenses/license.txt.
+ *      
+ * Devamatre reserves the right to modify the technical specifications and or 
+ * features without any prior notice.
+ *****************************************************************************/
 package com.rslakra.mock.logger;
 
 import java.io.File;
@@ -20,8 +39,12 @@ import org.apache.log4j.net.SyslogAppender;
 import com.rslakra.logger.Logger;
 
 /**
- *
- * @author Rohtash Singh (rohtash.singh@gmail.com)
+ * 
+ * @author Rohtash Lakra (rohtash.lakra@devamatre.com)
+ * @author Rohtash Singh Lakra (rohtash.singh@gmail.com)
+ * @created 2017-09-16 04:53:40 PM
+ * @version 1.0.0
+ * @since 1.0.0
  */
 public final class MockLogManager {
 	
