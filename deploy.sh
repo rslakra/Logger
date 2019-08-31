ef52c7df747fe7a61126c78364b2f1e8a0d2415d
Rohtash Singh Lakra <rohtash.singh@gmail.com>
2019-08-30 20:26:00 -0700

    Fixed the groupId.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LOCAL_MAVEN_REPO=~/.m2/repository/com/rslakra/dLogger
 export LOCAL_RELEASE_REPO=~/Documents/Workspaces/dGitHub/Releases
-mvn install:install-file -DgroupId=com.rslakra -DartifactId=dLogger -Dversion=1.0.0 -Dpackaging=jar -Dfile=target/dLogger-1.0-SNAPSHOT.jar -DlocalRepositoryPath=$LOCAL_RELEASE_REPO
+mvn install:install-file -DgroupId=com.devamatre -DartifactId=dLogger -Dversion=1.0.0 -Dpackaging=jar -Dfile=target/dLogger-1.0-SNAPSHOT.jar -DlocalRepositoryPath=$LOCAL_RELEASE_REPO
