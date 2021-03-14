58169641568793c103fd929f84071d2353d5db9e
Rohtash Lakra <rslakra@users.noreply.github.com>
2021-03-13 20:51:45 -0800

    reverted develop branch version

--- buildGradle.sh
+++ buildGradle.sh
@@ -2,5 +2,6 @@
 #Author: Rohtash Lakra
 echo "Building and publishing to local '~/.m2/repository' ..."
 gradle --daemon clean build publishToMavenLocal
+#gradle --daemon clean build maven-publish
 echo
 #gradle publishToMavenLocal
